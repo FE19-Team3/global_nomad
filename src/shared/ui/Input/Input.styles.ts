f81fb74b8601e567a52d56c69623b7f9ecc3c28f
Bangdayeon <dybang00@gmail.com>
2025-12-28 18:37:01 +0900

    feat: add input component (#32)

---
+++ src/shared/ui/Input/Input.styles.ts
@@ -0,0 +1,58 @@
+import { tv } from 'tailwind-variants';
+
+export const styles = tv({
+  slots: {
+    container: 'flex flex-col justify-start relative',
+    iconWrapper: 'absolute top-0 left-0 flex items-center justify-center pointer-events-none',
+    input:
+      'flex items-center w-full text-gray-950 placeholder-gray-400 focus:outline-none transition-all duration-150',
+    error: 'px-2 mt-1.5 text-red-500',
+  },
+  variants: {
+    variant: {
+      primary: {
+        input:
+          'border border-gray-100 bg-white shadow-[0px_2px_6px_0px_#00000005] focus:border-gray-500',
+      },
+      secondary: {
+        input:
+          'border-blue-50 bg-white shadow-[0px_6px_10px_0px_#0D99FF0D] focus:border focus:border-blue-200',
+      },
+    },
+    size: {
+      md: {
+        input: 'h-13.5 px-5',
+        iconWrapper: 'h-13.5 px-5',
+      },
+      lg: {
+        input: 'h-17.5 px-8',
+        iconWrapper: 'h-17.5 px-8',
+      },
+    },
+    radius: {
+      md: { input: 'rounded-[16px]' },
+      lg: { input: 'rounded-[24px]' },
+    },
+    disabled: {
+      true: { input: 'bg-gray-100 cursor-not-allowed opacity-50' },
+    },
+    error: {
+      true: { input: 'border border-red-500' },
+    },
+    hasIcon: {
+      true: { input: '' },
+    },
+  },
+  compoundVariants: [
+    {
+      hasIcon: true,
+      size: 'md',
+      class: { input: 'pl-14' },
+    },
+    {
+      hasIcon: true,
+      size: 'lg',
+      class: { input: 'pl-16' },
+    },
+  ],
+});
---
+++ src/shared/ui/Input/Input.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import { forwardRef, InputHTMLAttributes, ChangeEvent } from 'react';
+
+import { cn } from '@/shared/lib/cn';
+
+import { styles } from './Input.styles';
+
+export interface InputProps extends Omit<
+  InputHTMLAttributes<HTMLInputElement>,
+  'size' | 'value' | 'onChange' | 'type'
+> {
+  className?: string;
+  disabled?: boolean;
+  errorMsg?: string;
+  icon?: string; // TODO: SVGIcon 컴포넌트 추가후, 타입 변경
+  placeholder?: string;
+  radius?: 'md' | 'lg';
+  size?: 'md' | 'lg';
+  type?: 'text' | 'password';
+  value: string;
+  variant?: 'primary' | 'secondary';
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      className,
+      disabled = false,
+      errorMsg = '',
+      icon = '',
+      placeholder,
+      radius = 'md',
+      size = 'md',
+      type = 'text',
+      value,
+      variant,
+      onChange,
+      ...rest
+    },
+    ref,
+  ) => {
+    const {
+      container,
+      iconWrapper,
+      input,
+      error: errorClass,
+    } = styles({
+      size,
+      variant,
+      radius,
+      disabled,
+      error: !!errorMsg,
+      hasIcon: !!icon,
+    });
+
+    return (
+      <div className={container()}>
+        {icon && (
+          <div className={iconWrapper()}>
+            <img src="/file.svg" alt="아이콘" className="w-6 h-6" />{' '}
+            {/* TODO: SVGIcon 컴포넌트 추가 후, 형태 변경 */}
+          </div>
+        )}
+        <input
+          ref={ref}
+          type={type}
+          value={value}
+          placeholder={placeholder}
+          disabled={disabled}
+          className={cn(input(), className)}
+          onChange={onChange}
+          {...rest}
+        />
+        {errorMsg && <span className={errorClass()}>{errorMsg}</span>}
+      </div>
+    );
+  },
+);
+
+Input.displayName = 'Input';
+export default Input;
