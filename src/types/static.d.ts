31aa036a9bc4c034b32b13868874d39543ede5e0
daeya0406 <daeya0406@gmail.com>
2025-12-29 21:34:40 +0900

    feat: static.d에 png 추가(#36)

---
+++ src/types/static.d.ts
@@ -0,0 +1,25 @@
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.svg' {
+  import * as React from 'react';
+  const Component: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default Component;
+}
