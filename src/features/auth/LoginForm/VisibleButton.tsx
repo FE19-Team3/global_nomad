4cabb1f984dd778e8daf43843a87b1ecc8854f7c
Bangdayeon <dybang00@gmail.com>
2025-12-31 20:55:24 +0900

    feat: add loginform (#47)

---
+++ src/features/auth/LoginForm/VisibleButton.tsx
@@ -0,0 +1,22 @@
+import InVisible from '@/shared/assets/images/visibility_off.svg';
+import Visible from '@/shared/assets/images/visibility_on.svg';
+import Button from '@/shared/ui/Button/Button';
+
+interface VisibleButtonProps {
+  isVisible: boolean;
+  onClick: () => void;
+}
+const VisibleButton = ({ isVisible, onClick }: VisibleButtonProps) => {
+  return (
+    <Button
+      variant="icon"
+      iconOnly
+      onClick={onClick}
+      className="absolute right-0 top-0 m-1.5 text-gray-400"
+    >
+      <Button.Icon>{isVisible ? <InVisible /> : <Visible />}</Button.Icon>
+    </Button>
+  );
+};
+
+export default VisibleButton;
---
+++ src/stories/LoginForm.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+
+import LoginForm from '@/features/auth/LoginForm/LoginForm';
+
+const meta = {
+  title: 'Components/LoginForm',
+  component: LoginForm,
+  tags: ['autodocs'],
+  argTypes: {},
+  args: {},
+} satisfies Meta<typeof LoginForm>;
+
+export default meta;
+
+export const Default: StoryObj<typeof LoginForm> = {
+  render: () => {
+    const StoryComponent = () => {
+      return <LoginForm />;
+    };
+
+    return <StoryComponent />;
+  },
+};
