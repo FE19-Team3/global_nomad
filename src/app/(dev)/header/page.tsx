252c9d001deb29a562af6755075e6b3e9f880cc3
지니 <117137463+yujinimda@users.noreply.github.com>
2026-01-05 03:08:31 +0900

    feat: add clean header widgets (#126)
    * feat: add clean header widgets (#126)
    
    * feat: 주소 수정 (#126)

---
+++ src/app/(dev)/header/page.tsx
@@ -0,0 +1,51 @@
+import LogoFont from '@/shared/assets/logo/logo_font.svg';
+import LogoImg from '@/shared/assets/logo/logo_img.svg';
+import Button from '@/shared/ui/Button/Button';
+import { HeaderAuth } from '@/widgets/header/ui/HeaderAuth';
+import { HeaderGuest } from '@/widgets/header/ui/HeaderGuest';
+
+export default function Page() {
+  const mockUser = {
+    id: 1,
+    nickname: '정만철',
+    profileImageUrl: '',
+  };
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold">헤더 테스트</h1>
+
+      {/* 비로그인 */}
+      <div>
+        <h2 className="text-lg font-semibold mb-2">비로그인</h2>
+        <header className="sticky top-0 z-50 w-full h-18 bg-white">
+          <div className="max-w-[1520px] mx-auto h-full flex items-center justify-between px-4">
+            <Button as="link" href="/" variant="icon" iconOnly>
+              <LogoImg />
+              <div className="ml-3">
+                <LogoFont />
+              </div>
+            </Button>
+            <HeaderGuest />
+          </div>
+        </header>
+      </div>
+
+      {/* 로그인 */}
+      <div>
+        <h2 className="text-lg font-semibold mb-2">로그인</h2>
+        <header className="sticky top-0 z-50 w-full h-18 bg-white">
+          <div className="max-w-[1520px] mx-auto h-full flex items-center justify-between px-4">
+            <Button as="link" href="/" variant="icon" iconOnly>
+              <LogoImg />
+              <div className="ml-3">
+                <LogoFont />
+              </div>
+            </Button>
+            <HeaderAuth user={mockUser} />
+          </div>
+        </header>
+      </div>
+    </>
+  );
+}
--- src/app/(public)/main/page.tsx
+++ src/app/(public)/main/page.tsx
@@ -1,8 +1,8 @@
-import MainHero from './ui/MainHero';
+import MainHero from '@/widgets/Mainhero/MainHero';
 
 export default function MainPage() {
   return (
-    <main className="mx-auto max-w-346 px-8 py-26">
+    <main className="flex-1 mx-auto w-full max-w-350 px-10 py-26">
       <MainHero />
     </main>
   );
