e603b3c9ffab911f1dd7a078c365449cfeac0bbc
Bangdayeon <dybang00@gmail.com>
2026-01-05 21:26:40 +0900

    feat: signin page ui (#143)

---
+++ src/shared/ui/Select/index.tsx
@@ -0,0 +1,11 @@
+import SelectRoot from './Select';
+import { SelectContent } from './SelectContent';
+import { SelectItem } from './SelectItem';
+import { SelectTrigger } from './SelectTrigger';
+
+export const Select = {
+  Root: SelectRoot,
+  Trigger: SelectTrigger,
+  Content: SelectContent,
+  Item: SelectItem,
+};
