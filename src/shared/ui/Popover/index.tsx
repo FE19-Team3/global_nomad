727dc5c786b296c6913f52c4c62e88546f3c7dfd
Bangdayeon <dybang00@gmail.com>
2026-01-05 18:34:31 +0900

    feat: kebabmenu (#61)

---
+++ src/shared/ui/Popover/index.tsx
@@ -0,0 +1,8 @@
+import PopoverContent from './Content';
+import Popover from './Popover';
+import PopoverTrigger from './Trigger';
+
+export default Object.assign(Popover, {
+  Trigger: PopoverTrigger,
+  Content: PopoverContent,
+});
