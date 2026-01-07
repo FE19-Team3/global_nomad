29e44bcfe3b57c68616518ef280b615b1bcb6dbb
yujinimda <s0912135@gmail.com>
2026-01-07 14:59:43 +0900

    fix: 타입수정 (#130)

--- src/features/auth/model/useLoginForm.ts
+++ src/features/auth/model/useLoginForm.ts
@@ -2,12 +2,14 @@
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+import { ZodType } from 'zod'; // ZodType 추가
 
 import { LoginFormValues, LoginSchema } from '@/shared/schema/auth';
 
 export const useLoginForm = () => {
   const form = useForm<LoginFormValues>({
-    resolver: zodResolver(LoginSchema),
+    // LoginSchema를 ZodType<LoginFormValues>로 단언합니다.
+    resolver: zodResolver(LoginSchema as ZodType<LoginFormValues>),
     mode: 'onChange',
   });
   return form;
