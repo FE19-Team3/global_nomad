cce264e9ec16c3eb1e08bf3f31e3cacd376544a4
yujinimda <s0912135@gmail.com>
2026-01-07 14:18:15 +0900

    fix: 리조버 최신버전으로 수정 (#130)

--- src/features/auth/model/useLoginForm.ts
+++ src/features/auth/model/useLoginForm.ts
@@ -1,13 +1,12 @@
 'use client';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import type { ZodTypeAny } from 'zod';
 
 import { LoginSchema, type LoginFormValues } from '@/shared/schema/auth';
 
 export const useLoginForm = () => {
   const form = useForm<LoginFormValues>({
-    resolver: zodResolver(LoginSchema as ZodTypeAny),
+    resolver: zodResolver(LoginSchema),
     mode: 'onChange',
   });
 
