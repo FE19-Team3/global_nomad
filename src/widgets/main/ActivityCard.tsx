c792528ba89cf1999f13c7eecd66b796b070b9c2
yujinimda <s0912135@gmail.com>
2026-01-11 00:59:40 +0900

    fix: 경로수정 (#178)

--- src/widgets/main/ActivityCard.tsx
+++ src/widgets/main/ActivityCard.tsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from 'react';
 import type { Activity } from '@/entities/activity';
 import NoImageIcon from '@/shared/assets/icons/ic_no_image.svg';
 
-import StarIcon from '@/shared/assets/icons/icon-star-sm.png';
+import StarIcon from '@/shared/assets/icons/ic_star_sm.png';
 
 export const ActivityCard = ({ id, title, rating, reviewCount, price, imageUrl }: Activity) => {
   const [hasImage, setHasImage] = useState(Boolean(imageUrl));
