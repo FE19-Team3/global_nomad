56d134edca4fd7cbc51af5b8302765c71262ff47
nickwildee <nohyeog8@gmail.com>
2026-01-13 18:05:56 +0900

    chore: fetch api response에 상태코드, 헤더 추가 (#207)
    * chore: fetch api response에 상태코드, 헤더 추가 (#207)
    
    * docs: 불필요한 파일 제거 (#207)

--- src/shared/api/core/fetch-core.ts
+++ src/shared/api/core/fetch-core.ts
@@ -27,6 +27,12 @@ type CommonArgs = BaseOptions;
 type RequestJsonWithSchema<T> = RequestCoreOption & { schema: ZodType<T> };
 type RequestJsonWithoutSchema = RequestCoreOption & { schema?: undefined };
 
+type ResponseType<T> = {
+  data: T;
+  status: number;
+  headers: Headers;
+};
+
 export type RequesterEnv = {
   resolveUrl: (path: string, query?: Query) => string;
   getAccessToken?: () => Promise<string | undefined>;
@@ -70,15 +76,16 @@ export const createRequestCore = (env: RequesterEnv) => {
     return res;
   };
 
-  async function requestJson<T>(options: RequestJsonWithSchema<T>): Promise<T>;
-  async function requestJson(options: RequestJsonWithoutSchema): Promise<unknown>;
+  async function requestJson<T>(options: RequestJsonWithSchema<T>): Promise<ResponseType<T>>;
+  async function requestJson(options: RequestJsonWithoutSchema): Promise<ResponseType<unknown>>;
 
   async function requestJson<T>(
     options: RequestCoreOption & { schema?: ZodType<T> },
-  ): Promise<T | unknown> {
+  ): Promise<ResponseType<T> | ResponseType<unknown>> {
     try {
       const res = await requestRaw(options);
-      return await parseJsonResponse(res, { schema: options.schema });
+      const data = await parseJsonResponse(res, { schema: options.schema });
+      return { data, status: res.status, headers: res.headers };
     } catch (e) {
       throw toApiError(e);
     }
@@ -92,19 +99,20 @@ export const createRequestCore = (env: RequesterEnv) => {
     }
   };
 
-  const get = <T>(options: CommonArgs & { schema: ZodType<T> }): Promise<T> =>
+  const get = <T>(options: CommonArgs & { schema: ZodType<T> }): Promise<ResponseType<T>> =>
     requestJson<T>({ ...options, method: 'GET' });
 
-  const post = <T>(options: CommonArgs & { schema: ZodType<T> }): Promise<T> =>
+  const post = <T>(options: CommonArgs & { schema: ZodType<T> }): Promise<ResponseType<T>> =>
     requestJson<T>({ ...options, method: 'POST' });
 
-  const patch = <T>(options: CommonArgs & { schema: ZodType<T> }): Promise<T> =>
+  const patch = <T>(options: CommonArgs & { schema: ZodType<T> }): Promise<ResponseType<T>> =>
     requestJson<T>({ ...options, method: 'PATCH' });
 
   const del = (options: CommonArgs): Promise<void> => requestVoid({ ...options, method: 'DELETE' });
 
-  const upload = <T>(options: CommonArgs & { body: FormData; schema: ZodType<T> }): Promise<T> =>
-    requestJson<T>({ ...options, method: 'POST' });
+  const upload = <T>(
+    options: CommonArgs & { body: FormData; schema: ZodType<T> },
+  ): Promise<ResponseType<T>> => requestJson<T>({ ...options, method: 'POST' });
 
   return {
     requestJson,
