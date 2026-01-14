2df5fbb5aed35c83e8018880c78be857f713fa63
yujinimda <s0912135@gmail.com>
2026-01-15 01:35:20 +0900

    fix: 사용하지않는 엔티티 제거 (#223)

--- src/entities/activity/index.ts
+++ src/entities/activity/index.ts
@@ -1,5 +1,3 @@
 export type { Activity } from './model/activity.types';
-export type { ActivityDetail } from './model/activity-detail.types';
 export type { ActivitySchedule } from './model/activity-schedule.types';
 export type { ActivitySubImage } from './model/activity-image.types';
-export { mapActivityFromApi } from './model/activity.mapper';
