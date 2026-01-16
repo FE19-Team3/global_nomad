6d1a41154715b3f169767d80821524050b330ca4
daeya0406 <daeya0406@gmail.com>
2026-01-16 18:26:26 +0900

    feat: 빌드에러 확인(#235)

--- src/types/static.d.ts
+++ src/types/static.d.ts
@@ -23,23 +23,3 @@ declare module '*.svg' {
   const Component: React.FC<React.SVGProps<SVGSVGElement>>;
   export default Component;
 }
-
-declare global {
-  interface Window {
-    daum?: {
-      Postcode: new (options: {
-        oncomplete: (data: {
-          address?: string;
-          roadAddress?: string;
-          jibunAddress?: string;
-          bname?: string;
-          buildingName?: string;
-        }) => void;
-      }) => {
-        open: () => void;
-      };
-    };
-  }
-}
-
-export {};
